6d85c4d7e036d94a51c6132299077db926e5fbab
Odeyiany2 <miriamodeyianypeter@gmail.com>
2025-04-23 18:31:45 +0100

    Updated code to add logging functionality

--- assistant_core/llm_handler.py
+++ assistant_core/llm_handler.py
@@ -5,8 +5,18 @@
 import os
 from dotenv import load_dotenv
 from groq import Groq #language model wrapper for Groq API
-from langchain_community.document_loaders import DirectoryLoader #langchain wrapper for loading documents from a directory
+from langchain_community.document_loaders import DirectoryLoader, PyMuPDFLoader#langchain wrapper for loading documents from a directory
 from langchain.text_splitter import RecursiveCharacterTextSplitter #langchain wrapper for splitting text into smaller chunks
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain.vectorstores import Chroma 
+from langchain.chains.combine_documents import create_stuff_documents_chain 
+from langchain.chains.retrieval import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
+from assistant_core.logging import llm_handler_logger
+
+load_dotenv() #load environment variables from .env file
+
+# Set up the Groq API key
+groq_api_key = os.getenv("GROQ_API_KEY")
+if not groq_api_key:
+    raise ValueError("GROQ_API_KEY environment variable not set.")
---
+++ assistant_core/logging.py
@@ -0,0 +1,63 @@
+import logging
+import pathlib
+from pathlib import Path
+import os 
+
+cwd = Path.cwd()
+
+def setup_logger(
+        logger_name: str,
+        log_file: str,
+        log_level: int = logging.INFO
+) -> logging.Logger:
+    """
+    This function allows the system to create and write log data
+    of the system's operations.
+
+    Args:
+        logger_name (str): the name of the log file to create
+        log_file (str): file path to the log file
+        log_level (int): the value of the log type (warn, info, debug)
+    """
+
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(log_level)
+
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(log_level)
+    format = logging.Formatter('%(asctime)s - %(name)s- %(levelname)s - %(message)s')
+    file_handler.setFormatter(format)
+    logger.addHandler(file_handler)
+
+    return logger
+
+
+def create_folder_and_log_file(
+        folder_name:str,
+        file_name:str
+) -> pathlib.Path:
+    
+    new_path = cwd.joinpath(folder_name)
+
+    # create folder_path only once if not exist
+    new_path.mkdir(exist_ok=True)
+    log_file_path = new_path.joinpath(file_name)
+
+    # create file if not exist
+    log_file_path.touch()
+
+folder_name = "logs"
+
+log_files_to_create = [
+    "llm_handler.log",
+    "voice_handler.log",
+    "fastapi_app.log"]
+
+for log_file in log_files_to_create:
+    create_folder_and_log_file(folder_name, log_file)
+
+
+# Set up the loggers for each file
+llm_handler_logger = setup_logger("llm_handler", os.path.join(folder_name, "llm_handler.log"))
+voice_handler_logger = setup_logger("voice_handler", os.path.join(folder_name, "voice_handler.log"))
+fastapi_app_logger = setup_logger("fastapi_app", os.path.join(folder_name, "fastapi_app.log"))
