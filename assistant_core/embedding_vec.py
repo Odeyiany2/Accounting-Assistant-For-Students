b816c2244a1854b70fadeabb28d9f95dc55ab485
Odeyiany2 <miriamodeyianypeter@gmail.com>
2025-05-05 20:31:31 +0100

    Updated the embedding_vec.py file

--- assistant_core/embedding_vec.py
+++ assistant_core/embedding_vec.py
@@ -1,13 +1,18 @@
 #libraries to set up embedding model and vector store
 import os 
 import pinecone
+from pinecone import Pinecone
+import torch #library for GPU support
 from typing import List 
 from langchain.docstore.document import Document #langchain wrapper for document object
 from dotenv import load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter #langchain wrapper for splitting text into smaller chunks
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain.vectorstores import Chroma, Pinecone
+from assistant_core.doc_handler import load_documents_from_directory
 from config.logging import embedding_vec_logger
+
+
 
 
 load_dotenv() #load environment variables from .env file
@@ -18,8 +23,22 @@ if not pinecone_api_key:
     embedding_vec_logger.error("PINECONE_API_KEY environment variable not set.")
     raise ValueError("PINECONE_API_KEY environment variable not set.")
 
-#function for chunking 
+#initialize pinecone 
+pinecone.init(api_key = pinecone_api_key)
 
+#create index name and check for index existence
+index_name = "accounting-assistant"
+
+if index_name not in pinecone.list_indexes().names():
+    embedding_vec_logger.info(f"Creating index {index_name}...")
+    pinecone.create_index(index_name, 
+                          dimension=768, 
+                          metric="cosine")
+else:
+    embedding_vec_logger.info(f"Index {index_name} already exists.")
+
+
+#function for chunking 
 def chunk_docs(docs: List[Document], chunk_size: int = 1000, chunk_overlap: int = 200) -> List[Document]:
     """
     This function is used to chunk documents into smaller pieces for embedding. 
@@ -33,14 +52,44 @@ def chunk_docs(docs: List[Document], chunk_size: int = 1000, chunk_overlap: int 
     Returns:
         List[Document]: List of chunked documents.
     """
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size,
-        chunk_overlap=chunk_overlap,
-        length_function=len
-    )
-    documents = text_splitter.split_documents(docs)
-    embedding_vec_logger.info(f"Chunked {len(docs)} documents into {len(documents)} chunks.")
-    return documents
+    try:
+        if not docs:
+            print("No documents to chunk")
+            embedding_vec_logger.error("No documents to chunk.")
+            return []
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            length_function=len
+        )
+        documents = text_splitter.split_documents(docs)
+        embedding_vec_logger.info(f"Chunked {len(docs)} documents into {len(documents)} chunks.")
+        return documents
+    except Exception as e:
+        embedding_vec_logger.error(f"Error in chunking documents: {e}")
+        raise e
 
 #embedding model 
-embedding_model = HuggingFaceBgeEmbeddings()
+embedding_model = HuggingFaceBgeEmbeddings(
+    model_name = "sentence-transformers/all-MiniLM-L6-v2",
+    model_kwargs = {
+        "device": "cuda" if torch.cuda.is_available() else "cpu",
+        "max_length": 512,
+        "batch_size": 32
+    }
+)
+
+#getting the data extracted from the document handler module
+assistant_docs = load_documents_from_directory()
+
+#chunk the documents into smaller pieces
+chunked_docs = chunk_docs(assistant_docs)
+
+
+
+
+#push to pinecone index
+doc_store  = Pinecone.from_documents(chunked_docs, 
+                                     embedding_model, 
+                                     index_name = index_name)
+
