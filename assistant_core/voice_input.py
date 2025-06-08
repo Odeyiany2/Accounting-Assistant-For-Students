7eb82d3b11fae81f1f5753fcfe27854b358e462d
Odeyiany2 <miriamodeyianypeter@gmail.com>
2025-06-08 16:56:25 +0100

    Added voice input functionality to assistant_core This commit introduces a new voice input feature in the assistant_core module.

--- assistant_core/voice_input.py
+++ assistant_core/voice_input.py
@@ -1,1 +1,18 @@
 #voice implementation
+import speech_recognition as sr
+import os 
+from pydub import AudioSegment
+from config.logging import voice_input_logger
+
+def transcibe_audio(audio_file:str) -> str:
+    """
+    Transcribes audio from a file to text using the Google web speech API
+    Args:
+        audio_file (str): Path to the audio file to be transcribed.
+    """
+    try:
+        #initialize the recognizer 
+        recognizer = sr.Recognizer()
+    except Exception as e:
+        voice_input_logger.error(f"Error initializing recognizer: {e}")
+        raise e
